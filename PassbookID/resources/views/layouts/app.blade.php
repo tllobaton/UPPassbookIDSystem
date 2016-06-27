94b6585588a1a5479f46e1dbc7640527bc39e07c
Troi Lobaton <troivelobaton@gmail.com>
2016-06-27 16:25:06 +0800

    removed view users from logiut tab

--- PassbookID/resources/views/layouts/app.blade.php
+++ PassbookID/resources/views/layouts/app.blade.php
@@ -118,13 +118,12 @@
 
                             <ul class="dropdown-menu" role="menu">
 								@if (Auth::user()->adminstatus == 'yes')
-									<li><a href="{{ url('/AdminView') }}"><i class="fa fa-btn fa-users"></i>View List of Users</a></li>
+									<li><a href="{{ url('/AdminView') }}"><i class="fa fa-btn fa-exchange"></i>Switch to Admin View</a></li>
 									<li><a href="{{ url('/AdminCreate') }}"><i class="fa fa-btn fa-level-up"></i>Promote a User to Admin</a></li>
 								@endif
 								@if (Auth::user()->createstatus == 'yes')
 									<li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-eye"></i>View ID</a></li>
 									<li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-download"></i>Download ID</a></li>
-									<li><a href="{{ url('/AdminView') }}"><i class="fa fa-btn fa-exchange"></i>Switch to Admin View</a></li>
 								@endif
 								@if (Auth::user()->createstatus == 'yes' && Auth::user()->adminstatus == 'no')
 									<li><a href="{{ url('/UPD') }}"><i class="fa fa-btn fa-eye"></i>View Student ID</a></li>
