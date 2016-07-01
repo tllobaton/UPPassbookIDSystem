f7a15b1ab10c2465e1237a2635b5e120d4327147
Fulltime <Fulltime>
2016-07-01 16:49:14 +0800

    removed commented area in AdminController

--- PassbookID/app/Http/Controllers/AdminController.php
+++ PassbookID/app/Http/Controllers/AdminController.php
@@ -79,54 +79,6 @@ class AdminController extends Controller
 			$message = "Please select at least one record from the list of users.";
 			return view('AdminCreate', ['message'=>$message]);
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		/*if(is_array($checked)){						//if checkbox is ticked
-			foreach($checked as $cbox){
-				$db_name = DB::table('users')
-					->where('name', '=', $cbox->name)
-					->first();
-				if(!is_null($db_name)){
-					$db_stud = DB::table('users')
-						->where('name', '=', $cbox->name)
-						->value('createstatus');
-					if($db_stud=='no'){
-						$db_admin = DB::table('users')
-							->where('name', '=', $cbox->name)
-							->value('adminstatus');
-						if($db_admin=='no'){
-							DB::table('users')
-								->where('name', $name)
-								->update(array('adminstatus' => 'yes'));
-								$request->session()->flash('alert-success', 'The selected user has been promoted to an Administrator.');
-						}
-						else{
-							$message = "The selected user is already an Administrator.";
-							echo "<script type='text/javascript'>alert('$message');</script>";
-						}
-					}
-					else{
-						$message = "The user must have an employee status, and must not be a student.";
-						echo "<script type='text/javascript'>alert('$message');</script>";
-					}
-				}
-				else{
-					$message = "The user with the email " . $email . " does not exist.";
-					echo "<script type='text/javascript'>alert('$message');</script>";
-				}
-			}
-		}
-		else{
-		   $message = "Please select at least one record from the list of users.";
-		   echo "<script type='text/javascript'>alert('$message');</script>";
-		}*/
-		//return view('AdminCreate');
 	}
 	
 	public function showIdExpire() {
