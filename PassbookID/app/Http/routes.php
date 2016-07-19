367046a83f50357b13b56f64374614e651404ea3
Troi Lobaton <troivelobaton@gmail.com>
2016-07-19 16:25:58 +0800

    added admin middleware

--- PassbookID/app/Http/routes.php
+++ PassbookID/app/Http/routes.php
@@ -12,6 +12,8 @@
 */
 
 Route::auth();
+
+// Route for log-in
 Route::get('/', function () {
 	if (Auth::check()){
 		return Redirect::to('/Landing');
@@ -31,89 +33,54 @@ Route::get(
             ->response('png');
     }
 );
-Route::get('/UPVemp', function(){
-	return view('UPVemp');
-});
+
 Route::get('/redirect', 'SocialAuthController@redirect');
 Route::get('/callback', 'SocialAuthController@callback');
 
+// Route for admin methods. Middleware 'admin' means only admin can access route
+Route::group(['middleware' => ['auth', 'admin']], function () {
+	Route::get('/AdminView', 'AdminController@index');
+	Route::get('/AdminViewStud', 'AdminController@s_index');
+	Route::get('/AdminViewEmp', 'AdminController@e_index');
+
+	Route::get('/AdminCreate', 'AdminController@showPromoteView');
+	Route::get('/AdminRemove', 'AdminController@showRevokeView');
+
+	Route::get('/AdminExpire', 'AdminController@showIdExpire');
+	Route::post('/AdminExpire', 'AdminController@setIdExpire');
+
+	Route::get('/AdminCampDept', 'AdminController@showCampDept');
+	Route::post('/AdminCampDept', 'AdminController@addCampDeptBranch');
+
+	Route::get('/AdminAddUsers', 'AdminController@showAddUsers');
+	Route::post('/AdminAddUsers', 'AdminController@AddUsers');
+
+	Route::post('/AdminActDeactUsers', 'AdminController@AddDeactBranch');
+
+	Route::get('/AdminDeleteUsers', 'AdminController@showDeleteUsers');
+	Route::get('/SearchUser1', 'AdminController@search1');
+	
+	Route::post('/PromoteUser', 'AdminController@createAdmin');
+	Route::get('/SearchUser', 'AdminController@search');
+
+	Route::post('/RemoveAdmin', 'AdminController@removeAdmin');
+	Route::get('/SearchAdmin', 'AdminController@search_revoke');
+});
+
+
+// Route for everything else
 Route::group(['middleware' => 'auth'], function () {
-
-Route::get('/Details/{type?}', 'CreateIdController@showCreateIdDetails');
-
-Route::get('/Contacts/{type?}', 'CreateIdController@showCreateContacts');
-
-Route::get('/ViewEmergency/{type?}', 'CreateIdController@showEmergencyDetails');		//first back_of_id format; contains person to contact
-
-Route::get('/ViewEmergency1', 'CreateIdController@showEmergencyDetails1');		//second format; contains person to contact as well as barcode
-
-Route::get('/ViewEmergency2', 'CreateIdController@showEmergencyDetails2');		//third format for admin
-
-Route::get('/EmpDetails', 'CreateIdController@showCreateEmpDetails');
-
-Route::any('/Landing', 'CreateIdController@showLandingPage');
-Route::post('/FirstLanding', 'CreateIdController@FirstTimeLogin');
-
-Route::post('/Branch', 'CreateIdController@createIdBranch');
-
-Route::get('/ViewId/{type?}', 'CreateIdController@viewId');
-
-Route::get('/AdminView', 'AdminController@index');
-Route::get('/AdminViewStud', 'AdminController@s_index');
-Route::get('/AdminViewEmp', 'AdminController@e_index');
-
-Route::get('/AdminCreate', 'AdminController@showPromoteView');
-Route::get('/AdminRemove', 'AdminController@showRevokeView');
-
-Route::get('/AdminExpire', 'AdminController@showIdExpire');
-Route::post('/AdminExpire', 'AdminController@setIdExpire');
-
-Route::get('/AdminCampDept', 'AdminController@showCampDept');
-Route::post('/AdminCampDept', 'AdminController@addCampDeptBranch');
-
-Route::get('/AdminAddUsers', 'AdminController@showAddUsers');
-Route::post('/AdminAddUsers', 'AdminController@AddUsers');
-
-Route::post('/AdminActDeactUsers', 'AdminController@AddDeactBranch');
-
-Route::get('/AdminDeleteUsers', 'AdminController@showDeleteUsers');
-Route::get('/SearchUser1', 'AdminController@search1');
-
-Route::post('/CreateId', 'CreateIdController@createId');
-
-Route::post('/PromoteUser', 'AdminController@createAdmin');
-Route::get('/SearchUser', 'AdminController@search');
-
-Route::post('/RemoveAdmin', 'AdminController@removeAdmin');
-Route::get('/SearchAdmin', 'AdminController@search_revoke');
-
-Route::any('/Include', 'CreateIdController@test');
-
-Route::any('/MakePass/{type?}', 'CreateIdController@makePass');
-
-Route::any('/Include', 'CreateIdController@test');
-Route::get('/UPV', function(){
-	return view('UPV');
-});
-Route::get('/UPD', function(){
-	return view('UPD');
-});
-Route::get('/UPB', function(){
-	return view('UPB');
-});
-Route::get('/UPM', function(){
-	return view('UPM');
-});
-Route::get('/UPLB', function(){
-	return view('UPLB');
-});
-Route::get('/UPOU', function(){
-	return view('UPOU');
-});
-Route::get('/UPOU_admin', function(){
-	return view('UPOU_admin');
-});
-
-
-
+	Route::get('/Details/{type?}', 'CreateIdController@showCreateIdDetails');
+	Route::get('/Contacts/{type?}', 'CreateIdController@showCreateContacts');
+	Route::get('/ViewEmergency/{type?}', 'CreateIdController@showEmergencyDetails');	//first back_of_id format; contains person to contact
+	Route::get('/ViewEmergency1', 'CreateIdController@showEmergencyDetails1');			//second format; contains person to contact as well as barcode
+	Route::get('/ViewEmergency2', 'CreateIdController@showEmergencyDetails2');			//third format for admin
+	Route::get('/EmpDetails', 'CreateIdController@showCreateEmpDetails');
+	Route::any('/Landing', 'CreateIdController@showLandingPage');
+	Route::post('/FirstLanding', 'CreateIdController@FirstTimeLogin');
+	Route::post('/Branch', 'CreateIdController@createIdBranch');
+	Route::get('/ViewId/{type?}', 'CreateIdController@viewId');
+	Route::post('/CreateId', 'CreateIdController@createId');
+	Route::any('/Include', 'CreateIdController@test');
+	Route::any('/MakePass/{type?}', 'CreateIdController@makePass');
 });
